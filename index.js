17c540e1fb37cf943d4abb88dc776a7bfe1520b5
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-06-07 10:54:33 -0600

    commit tree 8601507020fdec8a8027e8e3781592e92ef1ed57

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(place = 'go to the office') {
+    return `This Monday, I will ${place}.`;
+}
+
+function wrapAdjective(result = '*') {
+    const innerFunction = function(adj = 'special') {
+        return `You are ${result}${adj}${result}!`;
+    }
+
+   return innerFunction;
+}
+
+wrapAdjective();
